c8acfdba77bd58bd85d4e72d8b664d73051d4261
Goutam Mahur <104672682+Goutam-Mahur@users.noreply.github.com>
2023-09-24 00:14:43 +0530

    slight changes in design

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,131 +1,158 @@
 import React from "react";
 import "./Login.css";
+import { useState, useEffect } from "react";
+
 
 function Login() {
+
+  const createUser = async() => {
+    // write the api logic 
+
+
+  } 
+
+
+  // setmail use state  {
+  const [email, setemail] = useState("Roll no./Email Address")
+  
+
+  // password & eye function {
+  const [password, setPassword] = useState("Enter Password");
+  const [type, setType] = useState("password");
+  const [icon, setIcon] = useState("fa-regular fa-eye-slash");
+
+  const handleToggle = () => {
+    if (type === "password") {
+      setIcon("fa-regular fa-eye");
+      setType("text");
+    } else {
+      setIcon("fa-regular fa-eye-slash");
+      setType("password");
+    }
+  };
+
+
+
   return (
     <div>
       <section className="vh-100">
-
-   
-        
-      {/* container  */}
+        {/* container  */}
         <div className="container-fluid h-custom">
-        {/* container 1st */}
+
+
+
+          {/* container 1st */}
           <div className="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-md-9 col-lg-6 col-xl-5">
-             {/* image  */}
-          <div className="pos position-relative rounded-circle"> {/* comment it to remove circle */}
-            <img src="./Image.svg"
-              className="img-fluid position-relative position-start-10" alt="test" />
-          </div>
-          </div>
+            <div className="col-md-9 col-lg-6 col-xl-5">
 
-          {/* container 2 */}
-          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
 
-            {/* form element in a container  */}
 
-            <form>
-              <div class="d-flex flex-row align-items-center justify-content-center my-4">
-                <p class="lead fw-normal text-center fs-3 ">Log in</p>
-              </div>
 
-              {/*  Email input*/}
-              <div className="form-outline mb-4">
-                <label className="form-label fs-6" for="form3Example3">
-                  Username
-                </label>
-                <input
-                  type="email"
-                  id="form3Example3"
-                  className="form-control form-control-lg border-dark rounded-border"
-                  placeholder="Enter a valid email address"
+              {/* image  */}
+              <div className="pos rounded-circle">
+                {/* comment it to remove circle */}
+                <img
+                  src="./Image.svg"
+                  className="img-fluid position-start-10"
+                  alt="test"
                 />
               </div>
+            </div>
 
 
-              {/*  Password input  */}
-              <div className="form-outline mb-3">
-                <label className="form-label fs-6" for="form3Example4">
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="form3Example4"
-                  className="form-control form-control-lg border-dark rounded-border"
-                  placeholder="Enter password"
-                />
-              </div>
 
-              {/*  Checkbox  */}
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="form-check mb-0">
-                  <input
-                    className="form-check-input me-2 rounded-circle border-dark rounded-border"
-                    type="checkbox"
-                    value=""
-                    id="form2Example3"
-                  />
-                  <label className="form-check-label" for="form2Example3">
-                    Remember me
-                  </label>
+
+            {/* container 2 */}
+            <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+              
+
+
+              {/* form element in a container  */}
+              <form>
+                <div className="d-flex flex-row align-items-center justify-content-center mb-3">
+                  <p className="lead fw-normal text-center fs-3">Log in</p>
                 </div>
-                <a href="#!" className="text-body">
-                  Forgot password?
-                </a>
-              </div>
 
 
-              {/* sign in button  */}
-              <div className="text-center text-lg-start mt-4 pt-2">
-                <button
-                  type="button"
-                  className="w-100 btn btn-dark btn-lg px-auto rounded-border"
-                >
-                  Sign in
-                </button>
 
 
-              {/* register */}
-                <p className="small fw-bold mt-2 pt-1 mb-0">
-                  Don't have an account?{" "}
-                  <a href="#!" className="link-primary p-2">
-                    Register
-                  </a>
-                </p>
-              </div>
+                {/*  Email input*/}
+                <div className="form-outline mb-4">
+                  <label className="form-label fs-6" htmlFor="form3Example3">
+                    Username
+                  </label>
+                  <input
+                    type="email"
+                    id="form3Example3"
+                    className="form-control form-control-lg border-dark rounded-pill"
+                    placeholder= {email}
+                    onChange={(event) => setemail(event.target.value)}
+                  />
+                </div>
 
 
-              {/* sign in with */}
-              <div className="divider d-flex align-items-center my-4">
-                <p className="text-center fw-bold mx-3 mb-0">Or</p>
-              </div>
 
-              <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                <p className="lead fw-normal mb-0 me-5">Sign in with</p>
-                <button
-                  type="button"
-                  className="btn btn-dark btn-floating mx-3 rounded-circle"
-                >
-                  <i className="fa-brands fa-google"></i>
-                </button>
 
-                <button
-                  type="button"
-                  className="btn btn-dark btn-floating mx-3 rounded-circle"
-                >
-                  <i className="fa fa-twitter"></i>
-                </button>
+                {/*  Password input  */}
+                <div className="form-outline mb-3 flex">
+                  <label className="form-label fs-6" htmlFor="form3Example4">
+                    Password
+                  </label>
+                  <div className="input-group mb-3">
+                    <input
+                      type={type}
+                      id="form3Example4"
+                      className="form-control form-control-lg border-custom"
+                      placeholder={password}
+                      onChange={(event) => setPassword(event.target.value)}
+                      autoComplete="current-password"
+                    />
+                    <span
+                      className="input-group-text border-dark rounded-end-pill bg-white"
+                      onClick={handleToggle}
+                    >
+                      <i className={`${icon}`} />
+                    </span>
+                  </div>
+                </div>
 
-                <button
-                  type="button"
-                  className="btn btn-dark btn-floating mx-3 rounded-circle"
-                >
-                  <i class="fa-brands fa-linkedin fa-lg"></i>
-                </button>
-              </div>
-            </form>
-          </div>
+                
+
+
+
+                {/*  Checkbox  */}
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="form-check mb-0">
+                    <input
+                      className="form-check-input me-2 rounded-circle border-dark rounded-border"
+                      type="checkbox"
+                      value=""
+                      id="form2Example3"
+                    />
+                    <label className="form-check-label" htmlFor="form2Example3">
+                      Remember me
+                    </label>
+                  </div>
+                </div>
+
+
+
+
+                {/* sign in button  */}
+                <div className="text-center text-lg-start mt-2 pt-2">
+                  <button
+                    type="button"
+                    className="w-100 btn btn-dark btn-lg px-auto rounded-pill"
+                    onClick = {createUser}
+                  >
+                    Sign in
+                  </button>
+                </div>
+
+
+
+              </form>
+            </div>
           </div>
         </div>
       </section>
